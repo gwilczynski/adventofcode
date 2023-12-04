4a6b0d91e96fcf43094a13aa43cca203c07b6c15
Grzegorz Wilczynski <292416+gwilczynski@users.noreply.github.com>
2023-12-04 10:52:03 +0100

    trebuchet (part 1)

---
+++ trebuchet/trebuchet.go
@@ -0,0 +1,38 @@
+package trebuchet
+
+import (
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func Decode(doc string) int {
+	var accumulator int
+	lines := split(doc)
+	for _, line := range lines {
+		first, last := calibration(line)
+		n, _ := number(first, last)
+
+		accumulator += n
+	}
+
+	return accumulator
+}
+
+func split(doc string) []string {
+	return strings.Split(doc, "\n")
+}
+
+func calibration(line string) (first string, last string) {
+	r := regexp.MustCompile("[0-9]")
+	numbers := r.FindAllString(line, -1)
+
+	first = numbers[0]
+	last = numbers[len(numbers)-1]
+
+	return
+}
+
+func number(first, second string) (int, error) {
+	return strconv.Atoi(first + second)
+}
