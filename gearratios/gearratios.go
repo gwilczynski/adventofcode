ab850cd708c691525207d8abac73c1b4f1f9d472
Grzegorz Wilczynski <292416+gwilczynski@users.noreply.github.com>
2023-12-10 17:24:08 +0100

    parse items

--- gearratios/gearratios.go
+++ gearratios/gearratios.go
@@ -1,5 +1,56 @@
 package gearratios
 
+import (
+	"regexp"
+	"strconv"
+)
+
 func GetPartNumbers(data []string) int {
+	for i, datum := range data {
+		scanItems(i, datum)
+	}
 	return 4361
+}
+
+func scanItems(index int, line string) (items []Item) {
+	pattern := `(\d+|[^\d.])`
+	re := regexp.MustCompile(pattern)
+	matches := re.FindAllStringSubmatchIndex(line, -1)
+
+	for _, match := range matches {
+		startIndex := match[0]
+		endIndex := match[1]
+		numberAsString := line[startIndex:endIndex]
+
+		value, err := strconv.Atoi(numberAsString)
+		itemType := Special
+		if err == nil {
+			itemType = Number
+		}
+
+		items = append(items, Item{
+			line:       index,
+			startIndex: startIndex,
+			endIndex:   endIndex - 1,
+			itemType:   itemType,
+			value:      value,
+		})
+	}
+
+	return
+}
+
+type ItemType string
+
+const (
+	Number  ItemType = "Number"
+	Special ItemType = "Special"
+)
+
+type Item struct {
+	line       int
+	startIndex int
+	endIndex   int
+	itemType   ItemType
+	value      int
 }
--- gearratios/gearratios_test.go
+++ gearratios/gearratios_test.go
@@ -1,6 +1,9 @@
 package gearratios
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestGetPartNumbers(t *testing.T) {
 	tests := []struct {
@@ -29,6 +32,89 @@ func TestGetPartNumbers(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := GetPartNumbers(tt.data); got != tt.want {
 				t.Errorf("GetPartNumbers() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_scanItems(t *testing.T) {
+	tests := []struct {
+		name      string
+		index     int
+		line      string
+		wantItems []Item
+	}{
+		{
+			name:  "first line",
+			index: 0,
+			line:  "467..114..",
+
+			wantItems: []Item{
+				{
+					line:       0,
+					startIndex: 0,
+					endIndex:   2,
+					itemType:   Number,
+					value:      467,
+				},
+				{
+					line:       0,
+					startIndex: 5,
+					endIndex:   7,
+					itemType:   Number,
+					value:      114,
+				},
+			},
+		},
+		{
+			name:  "second line",
+			index: 1,
+			line:  "...*......",
+
+			wantItems: []Item{
+				{
+					line:       1,
+					startIndex: 3,
+					endIndex:   3,
+					value:      0,
+					itemType:   Special,
+				},
+			},
+		},
+		{
+			name:  "third line",
+			index: 2,
+			line:  "....++.58.",
+
+			wantItems: []Item{
+				{
+					line:       2,
+					startIndex: 4,
+					endIndex:   4,
+					value:      0,
+					itemType:   Special,
+				},
+				{
+					line:       2,
+					startIndex: 5,
+					endIndex:   5,
+					value:      0,
+					itemType:   Special,
+				},
+				{
+					line:       2,
+					startIndex: 7,
+					endIndex:   8,
+					value:      58,
+					itemType:   Number,
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotItems := scanItems(tt.index, tt.line); !reflect.DeepEqual(gotItems, tt.wantItems) {
+				t.Errorf("scanItems() = %v, want %v", gotItems, tt.wantItems)
 			}
 		})
 	}
