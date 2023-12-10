4c030fea7b6cb8fe84d0ade7a461adbc29e3f9e3
Grzegorz Wilczynski <292416+gwilczynski@users.noreply.github.com>
2023-12-10 17:53:58 +0100

    collect items

--- gearratios/gearratios.go
+++ gearratios/gearratios.go
@@ -6,13 +6,17 @@ import (
 )
 
 func GetPartNumbers(data []string) int {
+	var numbers []NumberItem
+	var specials []SpecialItem
+
 	for i, datum := range data {
-		scanItems(i, datum)
+		scanItems(i, datum, &numbers, &specials)
 	}
+
 	return 4361
 }
 
-func scanItems(index int, line string) (items []Item) {
+func scanItems(index int, line string, numbers *[]NumberItem, specials *[]SpecialItem) {
 	pattern := `(\d+|[^\d.])`
 	re := regexp.MustCompile(pattern)
 	matches := re.FindAllStringSubmatchIndex(line, -1)
@@ -20,37 +24,38 @@ func scanItems(index int, line string) (items []Item) {
 	for _, match := range matches {
 		startIndex := match[0]
 		endIndex := match[1]
-		numberAsString := line[startIndex:endIndex]
+		valueAsString := line[startIndex:endIndex]
 
-		value, err := strconv.Atoi(numberAsString)
-		itemType := Special
+		value, err := strconv.Atoi(valueAsString)
 		if err == nil {
-			itemType = Number
+			*numbers = append(*numbers, NumberItem{
+				line:       index,
+				startIndex: startIndex,
+				endIndex:   endIndex - 1,
+				value:      value,
+			})
+		} else {
+			*specials = append(*specials, SpecialItem{
+				line:       index,
+				startIndex: startIndex,
+				endIndex:   endIndex - 1,
+				value:      valueAsString,
+			})
 		}
 
-		items = append(items, Item{
-			line:       index,
-			startIndex: startIndex,
-			endIndex:   endIndex - 1,
-			itemType:   itemType,
-			value:      value,
-		})
 	}
-
-	return
 }
 
-type ItemType string
-
-const (
-	Number  ItemType = "Number"
-	Special ItemType = "Special"
-)
-
-type Item struct {
+type NumberItem struct {
 	line       int
 	startIndex int
 	endIndex   int
-	itemType   ItemType
 	value      int
+}
+
+type SpecialItem struct {
+	line       int
+	startIndex int
+	endIndex   int
+	value      string
 }
--- gearratios/gearratios_test.go
+++ gearratios/gearratios_test.go
@@ -1,7 +1,6 @@
 package gearratios
 
 import (
-	"reflect"
 	"testing"
 )
 
@@ -37,47 +36,46 @@ func TestGetPartNumbers(t *testing.T) {
 	}
 }
 
-func Test_scanItems(t *testing.T) {
+func Test_scanItems1(t *testing.T) {
 	tests := []struct {
-		name      string
-		index     int
-		line      string
-		wantItems []Item
+		name     string
+		index    int
+		line     string
+		numbers  *[]NumberItem
+		specials *[]SpecialItem
 	}{
 		{
 			name:  "first line",
 			index: 0,
 			line:  "467..114..",
-
-			wantItems: []Item{
+			numbers: &[]NumberItem{
 				{
 					line:       0,
 					startIndex: 0,
 					endIndex:   2,
-					itemType:   Number,
 					value:      467,
 				},
 				{
 					line:       0,
 					startIndex: 5,
 					endIndex:   7,
-					itemType:   Number,
 					value:      114,
 				},
 			},
+			specials: nil,
 		},
 		{
 			name:  "second line",
 			index: 1,
 			line:  "...*......",
 
-			wantItems: []Item{
+			numbers: nil,
+			specials: &[]SpecialItem{
 				{
 					line:       1,
 					startIndex: 3,
 					endIndex:   3,
-					value:      0,
-					itemType:   Special,
+					value:      "*",
 				},
 			},
 		},
@@ -86,36 +84,63 @@ func Test_scanItems(t *testing.T) {
 			index: 2,
 			line:  "....++.58.",
 
-			wantItems: []Item{
-				{
-					line:       2,
-					startIndex: 4,
-					endIndex:   4,
-					value:      0,
-					itemType:   Special,
-				},
-				{
-					line:       2,
-					startIndex: 5,
-					endIndex:   5,
-					value:      0,
-					itemType:   Special,
-				},
+			numbers: &[]NumberItem{
 				{
 					line:       2,
 					startIndex: 7,
 					endIndex:   8,
 					value:      58,
-					itemType:   Number,
 				},
 			},
+			specials: &[]SpecialItem{
+				{
+					line:       2,
+					startIndex: 4,
+					endIndex:   4,
+					value:      "+",
+				},
+				{
+					line:       2,
+					startIndex: 5,
+					endIndex:   5,
+					value:      "+",
+				},
+			},
+		},
+		{
+			name:  "fourth line",
+			index: 3,
+			line:  "617*......",
+
+			numbers: &[]NumberItem{
+				{
+					line:       3,
+					startIndex: 0,
+					endIndex:   2,
+					value:      617,
+				},
+			},
+			specials: &[]SpecialItem{
+				{
+					line:       2,
+					startIndex: 3,
+					endIndex:   3,
+					value:      "+",
+				},
+			},
+		},
+		{
+			name:  "empty line",
+			index: 4,
+			line:  "..........",
+
+			numbers:  nil,
+			specials: nil,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if gotItems := scanItems(tt.index, tt.line); !reflect.DeepEqual(gotItems, tt.wantItems) {
-				t.Errorf("scanItems() = %v, want %v", gotItems, tt.wantItems)
-			}
+			scanItems(tt.index, tt.line, tt.numbers, tt.specials)
 		})
 	}
 }
