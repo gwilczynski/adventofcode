7cc249500b0f74da8f3902b5b83e884484272799
Grzegorz Wilczyński <292416+gwilczynski@users.noreply.github.com>
2024-12-02 14:01:43 +0100

    feat(historianhysteria): add functions for similarity scoring and data manipulation with tests

--- pkg/historianhysteria/historianhysteria.go
+++ pkg/historianhysteria/historianhysteria.go
@@ -19,6 +19,16 @@ func TotalDistance(data []string) int {
 	}, 0)
 }
 
+func SimilarityScore(data []string) int {
+	result := SplitLines(data, 2)
+	appears := Appearing(result)
+	multiplied := Multiplied(result[0], appears)
+
+	return lo.Reduce(multiplied, func(aggregator, item int, _ int) int {
+		return aggregator + item
+	}, 0)
+}
+
 func SplitLines(lines []string, numberOfColumns int) [][]int {
 	result := make([][]int, numberOfColumns)
 
@@ -52,6 +62,30 @@ func GetDistances(lists [][]int) []int {
 		for i := 0; i < len(list); i++ {
 			buff[i] = int(math.Abs(float64(buff[i] - list[i])))
 		}
+	}
+
+	return buff
+}
+
+func Appearing(lists [][]int) []int {
+	buff := make([]int, len(lists[0]))
+
+	for i := 0; i < len(lists[0]); i++ {
+		for j := 0; j < len(lists[1]); j++ {
+			if lists[0][i] == lists[1][j] {
+				buff[i]++
+			}
+		}
+	}
+
+	return buff
+}
+
+func Multiplied(lists []int, appeared []int) []int {
+	buff := make([]int, len(lists))
+
+	for i := 0; i < len(lists); i++ {
+		buff[i] = lists[i] * appeared[i]
 	}
 
 	return buff
--- pkg/historianhysteria/historianhysteria_test.go
+++ pkg/historianhysteria/historianhysteria_test.go
@@ -117,3 +117,85 @@ func TestGetDistances(t *testing.T) {
 		})
 	}
 }
+
+func TestSimilarityScore(t *testing.T) {
+	type args struct {
+		data []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "ok",
+			args: args{
+				data: []string{"3   4", "4   3", "2   5", "1   3", "3   9", "3   3"},
+			},
+			want: 31,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := historianhysteria.SimilarityScore(tt.args.data); got != tt.want {
+				t.Errorf("SimilarityScore() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestAppearing(t *testing.T) {
+	type args struct {
+		lists [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "ok",
+			args: args{
+				lists: [][]int{{3, 4, 2, 1, 3, 3}, {4, 3, 5, 3, 9, 3}},
+			},
+			want: []int{3, 1, 0, 0, 3, 3},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := historianhysteria.Appearing(tt.args.lists); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Appearing() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMultiplied(t *testing.T) {
+	type args struct {
+		lists    []int
+		appeared []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "ok",
+			args: args{
+				lists:    []int{3, 4, 2, 1, 3, 3},
+				appeared: []int{3, 1, 0, 0, 3, 3},
+			},
+			want: []int{9, 4, 0, 0, 9, 9},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := historianhysteria.Multiplied(tt.args.lists, tt.args.appeared); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Multiplied() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
