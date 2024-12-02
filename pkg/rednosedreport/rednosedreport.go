c163b2e3cdee8836a0ca86213adfa8e103630e7b
Grzegorz Wilczyński <292416+gwilczynski@users.noreply.github.com>
2024-12-02 19:42:40 +0100

    refactor(rednosedreport): rename and update functions for safety checks and integer conversion

--- pkg/rednosedreport/rednosedreport.go
+++ pkg/rednosedreport/rednosedreport.go
@@ -1,11 +1,40 @@
 package rednosedreport
 
-import "strings"
+import (
+	"strconv"
+	"strings"
+)
 
-func Safe(data []string) int {
+func HowManyReportsAreSafe(data []string) int {
 	return 0
 }
 
-func Split(data string) []string {
-	return strings.Fields(data)
+func Safe(data []int) bool {
+	increasing := 1
+	decreasing := 1
+
+	for i := 0; i < len(data)-1; i++ {
+		diff := data[i] - data[i+1]
+
+		if diff >= -3 && diff <= -1 {
+			increasing++
+		} else if diff >= 1 && diff <= 3 {
+			decreasing++
+		} else {
+			return false
+		}
+	}
+
+	return len(data) == increasing || len(data) == decreasing
+}
+
+func Split(data string) []int {
+	fields := strings.Fields(data)
+	numbers := make([]int, len(fields))
+
+	for i, field := range fields {
+		numbers[i], _ = strconv.Atoi(field)
+	}
+
+	return numbers
 }
--- pkg/rednosedreport/rednosedreport_test.go
+++ pkg/rednosedreport/rednosedreport_test.go
@@ -1,11 +1,13 @@
-package rednosedreport
+package rednosedreport_test
 
 import (
 	"reflect"
 	"testing"
+
+	"github.com/gwilczynski/adventofcode/pkg/rednosedreport"
 )
 
-func TestSafe(t *testing.T) {
+func TestHowManyReportsAreSafe(t *testing.T) {
 	type args struct {
 		data []string
 	}
@@ -25,7 +27,7 @@ func TestSafe(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := Safe(tt.args.data); got != tt.want {
+			if got := rednosedreport.HowManyReportsAreSafe(tt.args.data); got != tt.want {
 				t.Errorf("Safe() = %v, want %v", got, tt.want)
 			}
 		})
@@ -39,28 +41,90 @@ func TestSplit(t *testing.T) {
 	tests := []struct {
 		name string
 		args args
-		want []string
+		want []int
 	}{
 		{
 			name: "regular",
 			args: args{
 				data: "7 6 4 2 11 12",
 			},
-			want: []string{"7", "6", "4", "2", "11", "12"},
+			want: []int{7, 6, 4, 2, 11, 12},
 		},
 		{
 			name: "with additional spaces",
 			args: args{
 				data: "7  6   4    2      11 12     ",
 			},
-			want: []string{"7", "6", "4", "2", "11", "12"},
+			want: []int{7, 6, 4, 2, 11, 12},
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := Split(tt.args.data); !reflect.DeepEqual(got, tt.want) {
+			if got := rednosedreport.Split(tt.args.data); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("Split() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSafe(t *testing.T) {
+	type args struct {
+		data []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "safe because the levels are all decreasing by 1 or 2",
+			args: args{
+				data: []int{7, 6, 4, 2, 1},
+			},
+			want: true,
+		},
+		{
+			name: "unsafe because 2 7 is an increase of 5",
+			args: args{
+				data: []int{1, 2, 7, 8, 9},
+			},
+			want: false,
+		},
+		{
+			name: "unsafe because 6 2 is a decrease of 4",
+			args: args{
+				data: []int{9, 7, 6, 2, 1},
+			},
+			want: false,
+		},
+		{
+			name: "unsafe because 1 3 is increasing but 3 2 is decreasing",
+			args: args{
+				data: []int{1, 3, 2, 4, 5},
+			},
+			want: false,
+		},
+		{
+			name: "unsafe because 4 4 is neither an increase or a decrease",
+			args: args{
+				data: []int{8, 6, 4, 4, 1},
+			},
+			want: false,
+		},
+		{
+			name: "safe because the levels are all increasing by 1, 2, or 3",
+			args: args{
+				data: []int{1, 3, 6, 7, 9},
+			},
+			want: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := rednosedreport.Safe(tt.args.data); got != tt.want {
+				t.Errorf("Safe() = %v, want %v", got, tt.want)
 			}
 		})
 	}
