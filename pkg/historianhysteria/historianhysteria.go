c741239262d2006c79fb5099053620ed9faa2de4
Grzegorz Wilczyński <292416+gwilczynski@users.noreply.github.com>
2024-12-02 11:29:24 +0100

    feat(historianhysteria): add initial structure for distance calculation function

--- pkg/historianhysteria/historianhysteria.go
+++ pkg/historianhysteria/historianhysteria.go
@@ -1,5 +1,14 @@
 package historianhysteria
 
 func TotalDistance(data []string) int {
-	return 0
+	var distance int
+	// TODO, split lines into two collections
+
+	// TODO, sort all collections
+
+	// TODO, calculate distance between each pair
+
+	// TODO, sum all distances
+
+	return distance
 }
