dded8e9272fe189585199901d4b4dfabbd636ca0
Grzegorz Wilczyński <292416+gwilczynski@users.noreply.github.com>
2024-12-03 11:51:51 +0100

    feat: implement number extraction and multiplication logic in Call function

--- pkg/mullitover/mullitover.go
+++ pkg/mullitover/mullitover.go
@@ -1,9 +1,50 @@
 package mullitover
 
-import "fmt"
+import (
+	"regexp"
+	"strconv"
+	"strings"
+)
 
-func Call(data []string) int {
-	fmt.Println("len(data)", len(data))
+func Call(data []string) (sum int) {
+	records := make([]Num, 0, 1000)
 
-	return 0
+	for _, line := range data {
+		instructions := ExtractInstructions(line)
+		for _, instruction := range instructions {
+			n := ExtractNumbers(instruction)
+
+			records = append(records, n)
+		}
+	}
+
+	for _, record := range records {
+		sum += record.one * record.two
+	}
+
+	return
+}
+
+const pattern = `mul\(\d+,\d+\)`
+
+func ExtractInstructions(input string) []string {
+	r := regexp.MustCompile(pattern)
+
+	return r.FindAllString(input, -1)
+}
+
+func ExtractNumbers(input string) Num {
+	trimmed := strings.TrimPrefix(input, "mul(")
+	trimmed = strings.TrimSuffix(trimmed, ")")
+	parts := strings.Split(trimmed, ",")
+
+	num1, _ := strconv.Atoi(parts[0])
+	num2, _ := strconv.Atoi(parts[1])
+
+	return Num{num1, num2}
+}
+
+type Num struct {
+	one int
+	two int
 }
--- pkg/mullitover/mullitover_test.go
+++ pkg/mullitover/mullitover_test.go
@@ -1,6 +1,9 @@
 package mullitover
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestCall(t *testing.T) {
 	type args struct {
@@ -24,6 +27,68 @@ func TestCall(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := Call(tt.args.data); got != tt.want {
 				t.Errorf("Call() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestExtractInstructions(t *testing.T) {
+	type args struct {
+		input string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "only the four highlighted sections are real mul instructions",
+			args: args{
+				input: "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))",
+			},
+			want: []string{"mul(2,4)", "mul(5,5)", "mul(11,8)", "mul(8,5)"},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ExtractInstructions(tt.args.input); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ExtractInstructions() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestExtractNumbers(t *testing.T) {
+	type args struct {
+		input string
+	}
+	tests := []struct {
+		name string
+		args args
+		want Num
+	}{
+		{
+			name: "mul(2,4)",
+			args: args{
+				input: "mul(2,4)",
+			},
+			want: Num{2, 4},
+		},
+		{
+			name: "mul(11,3)",
+			args: args{
+				input: "mul(11,3)",
+			},
+			want: Num{11, 3},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := ExtractNumbers(tt.args.input)
+			if got != tt.want {
+				t.Errorf("ExtractNumbers() got = %v, want %v", got, tt.want)
 			}
 		})
 	}
