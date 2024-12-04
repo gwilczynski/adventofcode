66b0179d031bc3b40d7f11ec485be8791c6bb534
Grzegorz Wilczyński <292416+gwilczynski@users.noreply.github.com>
2024-12-04 12:25:34 +0100

    feat(ceressearch): add Cell struct with scanning logic and Matrix function

--- pkg/ceressearch/ceressearch.go
+++ pkg/ceressearch/ceressearch.go
@@ -1,9 +1,78 @@
 package ceressearch
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func Call(data []string) int {
 	fmt.Printf("Calling: %d\n", len(data))
 
 	return 0
+}
+
+type Cell struct {
+	Value  string
+	Row    int
+	Column int
+}
+
+func (c Cell) Scan(matrix [][]Cell) bool {
+	if c.Value == "X" {
+
+		// UP
+		if matrix[c.Column][c.Row-1].Value == "M" {
+			if matrix[c.Column][c.Row-2].Value == "A" {
+				if matrix[c.Column][c.Row-3].Value == "S" {
+					return true
+				}
+			}
+		}
+
+		// DOWN
+		if matrix[c.Column][c.Row+1].Value == "M" {
+			if matrix[c.Column][c.Row+2].Value == "A" {
+				if matrix[c.Column][c.Row+3].Value == "S" {
+					return true
+				}
+			}
+		}
+
+		// LEFT
+		if matrix[c.Column+1][c.Row].Value == "M" {
+			if matrix[c.Column+2][c.Row].Value == "A" {
+				if matrix[c.Column+3][c.Row].Value == "S" {
+					return true
+				}
+			}
+		}
+
+		// RIGHT
+		if matrix[c.Column-1][c.Row].Value == "M" {
+			if matrix[c.Column-2][c.Row].Value == "A" {
+				if matrix[c.Column-3][c.Row].Value == "S" {
+					return true
+				}
+			}
+		}
+	}
+
+	return false
+}
+
+func Matrix(data []string) [][]Cell {
+	matrix := make([][]Cell, len(data))
+
+	for i := 0; i < len(data); i++ {
+		letters := strings.Split(data[i], "")
+		matrix[i] = make([]Cell, len(letters))
+
+		for j := 0; j < len(letters); j++ {
+			matrix[i][j].Value = letters[j]
+			matrix[i][j].Row = i
+			matrix[i][j].Column = j
+		}
+	}
+
+	return matrix
 }
--- pkg/ceressearch/ceressearch_test.go
+++ pkg/ceressearch/ceressearch_test.go
@@ -1,6 +1,11 @@
-package ceressearch
+package ceressearch_test
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+
+	"github.com/gwilczynski/adventofcode/pkg/ceressearch"
+)
 
 func TestCall(t *testing.T) {
 	type args struct {
@@ -33,8 +38,63 @@ func TestCall(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := Call(tt.args.data); got != tt.want {
+			if got := ceressearch.Call(tt.args.data); got != tt.want {
 				t.Errorf("Call() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMatrix(t *testing.T) {
+	type args struct {
+		data []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]ceressearch.Cell
+	}{
+		{
+			name: "ok",
+			args: args{
+				data: []string{
+					"AB",
+					"CD",
+				},
+			},
+			want: [][]ceressearch.Cell{
+				{
+					ceressearch.Cell{
+						Value:  "A",
+						Row:    0,
+						Column: 0,
+					},
+					ceressearch.Cell{
+						Value:  "B",
+						Row:    0,
+						Column: 1,
+					},
+				},
+				{
+					ceressearch.Cell{
+						Value:  "C",
+						Row:    1,
+						Column: 0,
+					},
+					ceressearch.Cell{
+						Value:  "D",
+						Row:    1,
+						Column: 1,
+					},
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ceressearch.Matrix(tt.args.data); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Matrix() = %v, want %v", got, tt.want)
 			}
 		})
 	}
