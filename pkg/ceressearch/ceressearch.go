222ad74e7c0c1cd1684e431387a3458c5d0a1240
Grzegorz Wilczyński <292416+gwilczynski@users.noreply.github.com>
2024-12-04 13:35:51 +0100

    refactor: simplify Call function and add boundary checks in Scan method

--- pkg/ceressearch/ceressearch.go
+++ pkg/ceressearch/ceressearch.go
@@ -1,14 +1,22 @@
 package ceressearch
 
 import (
-	"fmt"
 	"strings"
 )
 
 func Call(data []string) int {
-	fmt.Printf("Calling: %d\n", len(data))
+	var counter int
+	matrix := Matrix(data)
 
-	return 0
+	for i := 0; i < len(matrix); i++ {
+		for j := 0; j < len(matrix[i]); j++ {
+			if ok := matrix[i][j].Scan(matrix); ok {
+				counter++
+			}
+		}
+	}
+
+	return counter
 }
 
 type Cell struct {
@@ -18,39 +26,42 @@ type Cell struct {
 }
 
 func (c Cell) Scan(matrix [][]Cell) bool {
+	numberOfColumns := len(matrix)
+	numberOfRows := len(matrix[0])
+
 	if c.Value == "X" {
 
 		// UP
-		if matrix[c.Column][c.Row-1].Value == "M" {
-			if matrix[c.Column][c.Row-2].Value == "A" {
-				if matrix[c.Column][c.Row-3].Value == "S" {
+		if c.Row-1 >= 0 && matrix[c.Column][c.Row-1].Value == "M" {
+			if c.Row-2 >= 0 && matrix[c.Column][c.Row-2].Value == "A" {
+				if c.Row-3 >= 0 && matrix[c.Column][c.Row-3].Value == "S" {
 					return true
 				}
 			}
 		}
 
 		// DOWN
-		if matrix[c.Column][c.Row+1].Value == "M" {
-			if matrix[c.Column][c.Row+2].Value == "A" {
-				if matrix[c.Column][c.Row+3].Value == "S" {
+		if c.Row+1 < numberOfRows && matrix[c.Column][c.Row+1].Value == "M" {
+			if c.Row+2 < numberOfRows && matrix[c.Column][c.Row+2].Value == "A" {
+				if c.Row+3 < numberOfRows && matrix[c.Column][c.Row+3].Value == "S" {
 					return true
 				}
 			}
 		}
 
 		// LEFT
-		if matrix[c.Column+1][c.Row].Value == "M" {
-			if matrix[c.Column+2][c.Row].Value == "A" {
-				if matrix[c.Column+3][c.Row].Value == "S" {
+		if c.Column+1 < numberOfColumns && matrix[c.Column+1][c.Row].Value == "M" {
+			if c.Column+2 < numberOfColumns && matrix[c.Column+2][c.Row].Value == "A" {
+				if c.Column+3 < numberOfColumns && matrix[c.Column+3][c.Row].Value == "S" {
 					return true
 				}
 			}
 		}
 
 		// RIGHT
-		if matrix[c.Column-1][c.Row].Value == "M" {
-			if matrix[c.Column-2][c.Row].Value == "A" {
-				if matrix[c.Column-3][c.Row].Value == "S" {
+		if c.Column-1 >= 0 && matrix[c.Column-1][c.Row].Value == "M" {
+			if c.Column-2 >= 0 && matrix[c.Column-2][c.Row].Value == "A" {
+				if c.Column-3 >= 0 && matrix[c.Column-3][c.Row].Value == "S" {
 					return true
 				}
 			}
@@ -68,9 +79,11 @@ func Matrix(data []string) [][]Cell {
 		matrix[i] = make([]Cell, len(letters))
 
 		for j := 0; j < len(letters); j++ {
-			matrix[i][j].Value = letters[j]
-			matrix[i][j].Row = i
-			matrix[i][j].Column = j
+			matrix[i][j] = Cell{
+				Value:  letters[j],
+				Row:    i,
+				Column: j,
+			}
 		}
 	}
 
