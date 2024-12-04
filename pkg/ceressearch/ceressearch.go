03dea7dbaba86646d5310cf8dfbf923edb484cdc
Grzegorz Wilczyński <292416+gwilczynski@users.noreply.github.com>
2024-12-04 14:45:54 +0100

    feat: add directional scan functionality and improve cell struct

--- pkg/ceressearch/ceressearch.go
+++ pkg/ceressearch/ceressearch.go
@@ -1,6 +1,7 @@
 package ceressearch
 
 import (
+	"fmt"
 	"strings"
 )
 
@@ -10,24 +11,42 @@ func Call(data []string) int {
 
 	for i := 0; i < len(matrix); i++ {
 		for j := 0; j < len(matrix[i]); j++ {
-			if ok := matrix[i][j].Scan(matrix); ok {
-				counter++
+			result := matrix[i][j].Scan(matrix)
+			if result > 0 {
+				fmt.Println(result)
 			}
+
+			counter += result
 		}
 	}
 
 	return counter
 }
 
+type CellType string
+
+const (
+	Up        CellType = "UP"
+	UpRight   CellType = "UP-RIGHT"
+	Right     CellType = "RIGHT"
+	RightDown CellType = "RIGHT-DOWN"
+	Down      CellType = "DOWN"
+	DownLeft  CellType = "DOWN-LEFT"
+	Left      CellType = "LEFT"
+	LeftUp    CellType = "LEFT-UP"
+)
+
 type Cell struct {
 	Value  string
 	Row    int
 	Column int
+	Types  []CellType
 }
 
-func (c Cell) Scan(matrix [][]Cell) bool {
+func (c *Cell) Scan(matrix [][]*Cell) int {
 	numberOfColumns := len(matrix)
 	numberOfRows := len(matrix[0])
+	var counter int
 
 	if c.Value == "X" {
 
@@ -35,7 +54,37 @@ func (c Cell) Scan(matrix [][]Cell) bool {
 		if c.Row-1 >= 0 && matrix[c.Column][c.Row-1].Value == "M" {
 			if c.Row-2 >= 0 && matrix[c.Column][c.Row-2].Value == "A" {
 				if c.Row-3 >= 0 && matrix[c.Column][c.Row-3].Value == "S" {
-					return true
+					c.Types = append(c.Types, Up)
+					counter++
+				}
+			}
+		}
+		// UP-RIGHT
+		if c.Column+1 < numberOfColumns && c.Row-1 >= 0 && matrix[c.Column+1][c.Row-1].Value == "M" {
+			if c.Column+2 < numberOfColumns && c.Row-2 >= 0 && matrix[c.Column+2][c.Row-2].Value == "A" {
+				if c.Column+3 < numberOfColumns && c.Row-3 >= 0 && matrix[c.Column+3][c.Row-3].Value == "S" {
+					c.Types = append(c.Types, UpRight)
+					counter++
+				}
+			}
+		}
+
+		// RIGHT
+		if c.Column+1 < numberOfColumns && matrix[c.Column+1][c.Row].Value == "M" {
+			if c.Column+2 < numberOfColumns && matrix[c.Column+2][c.Row].Value == "A" {
+				if c.Column+3 < numberOfColumns && matrix[c.Column+3][c.Row].Value == "S" {
+					c.Types = append(c.Types, Right)
+					counter++
+				}
+			}
+		}
+
+		// RIGHT-DOWN
+		if c.Column+1 < numberOfColumns && c.Row+1 < numberOfRows && matrix[c.Column+1][c.Row+1].Value == "M" {
+			if c.Column+2 < numberOfColumns && c.Row+2 < numberOfRows && matrix[c.Column+2][c.Row+2].Value == "A" {
+				if c.Column+3 < numberOfColumns && c.Row+3 < numberOfRows && matrix[c.Column+3][c.Row+3].Value == "S" {
+					c.Types = append(c.Types, RightDown)
+					counter++
 				}
 			}
 		}
@@ -44,45 +93,59 @@ func (c Cell) Scan(matrix [][]Cell) bool {
 		if c.Row+1 < numberOfRows && matrix[c.Column][c.Row+1].Value == "M" {
 			if c.Row+2 < numberOfRows && matrix[c.Column][c.Row+2].Value == "A" {
 				if c.Row+3 < numberOfRows && matrix[c.Column][c.Row+3].Value == "S" {
-					return true
+					c.Types = append(c.Types, Down)
+					counter++
+				}
+			}
+		}
+
+		// DOWN-LEFT
+		if c.Column-1 >= 0 && c.Row+1 < numberOfRows && matrix[c.Column-1][c.Row+1].Value == "M" {
+			if c.Column-2 >= 0 && c.Row+2 < numberOfRows && matrix[c.Column-2][c.Row+2].Value == "A" {
+				if c.Column-3 >= 0 && c.Row+3 < numberOfRows && matrix[c.Column-3][c.Row+3].Value == "S" {
+					c.Types = append(c.Types, DownLeft)
+					counter++
 				}
 			}
 		}
 
 		// LEFT
-		if c.Column+1 < numberOfColumns && matrix[c.Column+1][c.Row].Value == "M" {
-			if c.Column+2 < numberOfColumns && matrix[c.Column+2][c.Row].Value == "A" {
-				if c.Column+3 < numberOfColumns && matrix[c.Column+3][c.Row].Value == "S" {
-					return true
+		if c.Column-1 >= 0 && matrix[c.Column-1][c.Row].Value == "M" {
+			if c.Column-2 >= 0 && matrix[c.Column-2][c.Row].Value == "A" {
+				if c.Column-3 >= 0 && matrix[c.Column-3][c.Row].Value == "S" {
+					c.Types = append(c.Types, Left)
+					counter++
 				}
 			}
 		}
 
-		// RIGHT
-		if c.Column-1 >= 0 && matrix[c.Column-1][c.Row].Value == "M" {
-			if c.Column-2 >= 0 && matrix[c.Column-2][c.Row].Value == "A" {
-				if c.Column-3 >= 0 && matrix[c.Column-3][c.Row].Value == "S" {
-					return true
+		// LEFT-UP
+		if c.Column-1 >= 0 && c.Row-1 >= 0 && matrix[c.Column-1][c.Row-1].Value == "M" {
+			if c.Column-2 >= 0 && c.Row-2 >= 0 && matrix[c.Column-2][c.Row-2].Value == "A" {
+				if c.Column-3 >= 0 && c.Row-3 >= 0 && matrix[c.Column-3][c.Row-3].Value == "S" {
+					c.Types = append(c.Types, LeftUp)
+					counter++
 				}
 			}
 		}
 	}
 
-	return false
+	return counter
 }
 
-func Matrix(data []string) [][]Cell {
-	matrix := make([][]Cell, len(data))
+func Matrix(data []string) [][]*Cell {
+	matrix := make([][]*Cell, len(data))
 
 	for i := 0; i < len(data); i++ {
 		letters := strings.Split(data[i], "")
-		matrix[i] = make([]Cell, len(letters))
+		matrix[i] = make([]*Cell, len(letters))
 
 		for j := 0; j < len(letters); j++ {
-			matrix[i][j] = Cell{
+			matrix[i][j] = &Cell{
 				Value:  letters[j],
-				Row:    i,
-				Column: j,
+				Row:    j,
+				Column: i,
+				Types:  make([]CellType, 0),
 			}
 		}
 	}
