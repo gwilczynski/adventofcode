45113902d0f9a0f22c40e9aa18b43e0bfcc0f7e0
Grzegorz Wilczyński <292416+gwilczynski@users.noreply.github.com>
2024-12-04 11:41:48 +0100

    test: add unit test for Call function in ceressearch package

---
+++ pkg/ceressearch/ceressearch_test.go
@@ -0,0 +1,41 @@
+package ceressearch
+
+import "testing"
+
+func TestCall(t *testing.T) {
+	type args struct {
+		data []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "ok",
+			args: args{
+				data: []string{
+					"MMMSXXMASM",
+					"MSAMXMSMSA",
+					"AMXSXMAAMM",
+					"MSAMASMSMX",
+					"XMASAMXAMM",
+					"XXAMMXXAMA",
+					"SMSMSASXSS",
+					"SAXAMASAAA",
+					"MAMMMXMMMM",
+					"MXMXAXMASX",
+				},
+			},
+			want: 18,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Call(tt.args.data); got != tt.want {
+				t.Errorf("Call() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
