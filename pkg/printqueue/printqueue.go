b9b52cced9f9c8f06f354088f12ef8fd4717a85e
Grzegorz Wilczyński <292416+gwilczynski@users.noreply.github.com>
2024-12-05 14:17:11 +0100

    feat(printqueue): add RulesAndUpdates function with tests for page ordering logic

--- pkg/printqueue/printqueue.go
+++ pkg/printqueue/printqueue.go
@@ -1,5 +1,67 @@
 package printqueue
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 func Call(data []string) int {
+	rules, updates := RulesAndUpdates(data)
+	fmt.Println(rules, updates)
+
 	return len(data)
+}
+
+type Rule struct {
+	before, after int
+}
+
+const (
+	ModeRule = iota
+	ModeUpdate
+)
+
+func RulesAndUpdates(data []string) ([]Rule, [][]int) {
+	// page ordering rules
+	var rules []Rule
+
+	// pages to produce in each update
+	var updates [][]int
+
+	mode := ModeRule
+
+	for _, line := range data {
+		if line == "" {
+			mode = ModeUpdate
+			continue
+		}
+
+		if mode == ModeRule {
+			rule := strings.Split(line, "|")
+			rules = append(rules, Rule{before: toInt(rule[0]), after: toInt(rule[1])})
+		}
+
+		if mode == ModeUpdate {
+			updatesInLine := strings.Split(line, ",")
+			updateInt := make([]int, len(updatesInLine))
+
+			for i := 0; i < len(updatesInLine); i++ {
+				updateInt[i] = toInt(updatesInLine[i])
+			}
+
+			updates = append(updates, updateInt)
+		}
+	}
+
+	return rules, updates
+}
+
+func toInt(value string) (i int) {
+	i, err := strconv.Atoi(value)
+	if err != nil {
+		panic(err)
+	}
+
+	return i
 }
--- pkg/printqueue/printqueue_test.go
+++ pkg/printqueue/printqueue_test.go
@@ -1,6 +1,9 @@
 package printqueue
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestCall(t *testing.T) {
 	type args struct {
@@ -52,6 +55,61 @@ func TestCall(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := Call(tt.args.data); got != tt.want {
 				t.Errorf("Call() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestRulesAndUpdates(t *testing.T) {
+	type args struct {
+		data []string
+	}
+	tests := []struct {
+		name        string
+		args        args
+		wantRules   []Rule
+		wantUpdates [][]int
+	}{
+		{
+			name: "ok",
+			args: args{
+				data: []string{
+					"47|53",
+					"97|13",
+					"",
+					"75,47,61,53,29",
+					"75,29,13",
+				},
+			},
+			wantRules: []Rule{
+				{
+					before: 47,
+					after:  53,
+				},
+				{
+					before: 97,
+					after:  13,
+				},
+			},
+			wantUpdates: [][]int{
+				{
+					75, 47, 61, 53, 29,
+				},
+				{
+					75, 29, 13,
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			rules, updates := RulesAndUpdates(tt.args.data)
+			if !reflect.DeepEqual(rules, tt.wantRules) {
+				t.Errorf("RulesAndUpdates() got = %v, want %v", rules, tt.wantRules)
+			}
+			if !reflect.DeepEqual(updates, tt.wantUpdates) {
+				t.Errorf("RulesAndUpdates() got1 = %v, want %v", updates, tt.wantUpdates)
 			}
 		})
 	}
