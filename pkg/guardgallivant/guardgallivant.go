40df0effc16ebb4e8e26b3816577fa05a7ecc75b
Grzegorz Wilczyński <292416+gwilczynski@users.noreply.github.com>
2024-12-06 13:12:42 +0100

    feat: add DecodeLine function and associated tests

--- pkg/guardgallivant/guardgallivant.go
+++ pkg/guardgallivant/guardgallivant.go
@@ -1,5 +1,66 @@
 package guardgallivant
 
+import "strings"
+
 func Call(data []string) int {
 	return len(data)
+}
+
+func DecodeLine(line string, j int) ([]*Field, *Guard) {
+	elements := strings.Split(line, "")
+	fields := make([]*Field, len(elements))
+	var guard *Guard
+
+	for i, element := range elements {
+		if element == "." {
+			fields[i] = &Field{T: Free}
+		}
+		if element == "#" {
+			fields[i] = &Field{T: Obstruction}
+		}
+		if element == "^" {
+			fields[i] = &Field{T: Free}
+			guard = &Guard{
+				D: Up,
+				P: Position{
+					I: i,
+					J: j,
+				},
+			}
+		}
+	}
+
+	return fields, guard
+}
+
+type FieldType string
+
+const (
+	Free        FieldType = "."
+	Obstruction FieldType = "#"
+)
+
+type Field struct {
+	T FieldType
+}
+
+type Direction string
+
+const (
+	Up    Direction = "up"
+	Down  Direction = "down"
+	Right Direction = "right"
+	Left  Direction = "left"
+)
+
+type Position struct {
+	I, J int
+}
+type Guard struct {
+	D Direction
+	P Position
+}
+
+func (g Guard) Flight() (newPosition Position) {
+	return g.P
 }
---
+++ pkg/guardgallivant/guardgallivant_test.go
@@ -0,0 +1,120 @@
+package guardgallivant_test
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/gwilczynski/adventofcode/pkg/guardgallivant"
+)
+
+func TestDecodeLine(t *testing.T) {
+	type args struct {
+		line string
+		j    int
+	}
+	tests := []struct {
+		name   string
+		args   args
+		fields []*guardgallivant.Field
+		guard  *guardgallivant.Guard
+	}{
+		{
+			name: "ok",
+			args: args{
+				line: "....#.....",
+				j:    0,
+			},
+			fields: []*guardgallivant.Field{
+				{
+					T: guardgallivant.Free,
+				},
+				{
+					T: guardgallivant.Free,
+				},
+				{
+					T: guardgallivant.Free,
+				},
+				{
+					T: guardgallivant.Free,
+				},
+				{
+					T: guardgallivant.Obstruction,
+				},
+				{
+					T: guardgallivant.Free,
+				},
+				{
+					T: guardgallivant.Free,
+				},
+				{
+					T: guardgallivant.Free,
+				},
+				{
+					T: guardgallivant.Free,
+				},
+				{
+					T: guardgallivant.Free,
+				},
+			},
+			guard: nil,
+		},
+		{
+			name: "ok",
+			args: args{
+				line: ".#..^.....",
+				j:    6,
+			},
+			fields: []*guardgallivant.Field{
+				{
+					T: guardgallivant.Free,
+				},
+				{
+					T: guardgallivant.Obstruction,
+				},
+				{
+					T: guardgallivant.Free,
+				},
+				{
+					T: guardgallivant.Free,
+				},
+				{
+					T: guardgallivant.Free,
+				},
+				{
+					T: guardgallivant.Free,
+				},
+				{
+					T: guardgallivant.Free,
+				},
+				{
+					T: guardgallivant.Free,
+				},
+				{
+					T: guardgallivant.Free,
+				},
+				{
+					T: guardgallivant.Free,
+				},
+			},
+			guard: &guardgallivant.Guard{
+				D: guardgallivant.Up,
+				P: guardgallivant.Position{
+					I: 4,
+					J: 6,
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			fields, guard := guardgallivant.DecodeLine(tt.args.line, tt.args.j)
+			if !reflect.DeepEqual(fields, tt.fields) {
+				t.Errorf("DecodeLine() fields = %v, want %v", fields, tt.fields)
+			}
+			if !reflect.DeepEqual(guard, tt.guard) {
+				t.Errorf("DecodeLine() guard = %v, want %v", guard, tt.guard)
+			}
+		})
+	}
+}
