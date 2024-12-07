c7686c164b1fecb72d42d0106b1a726c599c52dc
Grzegorz Wilczyński <292416+gwilczynski@users.noreply.github.com>
2024-12-07 11:37:51 +0100

    feat(bridgerepair): add Puzzle parsing logic and unit tests

--- pkg/bridgerepair/bridgerepair.go
+++ pkg/bridgerepair/bridgerepair.go
@@ -1,5 +1,41 @@
 package bridgerepair
 
+import (
+	"strconv"
+	"strings"
+)
+
 func Call(data []string) int {
 	return len(data)
+}
+
+type Puzzle struct {
+	Result  int
+	Numbers []int
+}
+
+func Parse(data []string) []Puzzle {
+	puzzles := make([]Puzzle, len(data))
+
+	// for _, d := range data {
+	// }
+
+	return puzzles
+}
+
+func ParseLine(line string) *Puzzle {
+	parts := strings.Split(line, ":")
+	result, _ := strconv.Atoi(parts[0])
+
+	rest := strings.Split(strings.TrimSpace(parts[1]), " ")
+	numbers := make([]int, len(rest))
+
+	for i, s := range rest {
+		numbers[i], _ = strconv.Atoi(s)
+	}
+
+	return &Puzzle{
+		Result:  result,
+		Numbers: numbers,
+	}
 }
---
+++ pkg/bridgerepair/bridgerepair_test.go
@@ -0,0 +1,83 @@
+package bridgerepair
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestCall(t *testing.T) {
+	type args struct {
+		data []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "ok",
+			args: args{
+				data: []string{
+					"190: 10 19",
+					"3267: 81 40 27",
+					"83: 17 5",
+					"156: 15 6",
+					"7290: 6 8 6 15",
+					"161011: 16 10 13",
+					"192: 17 8 14",
+					"21037: 9 7 18 13",
+					"292: 11 6 16 20",
+				},
+			},
+			want: 3749,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Call(tt.args.data); got != tt.want {
+				t.Errorf("Call() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestParseLine(t *testing.T) {
+	type args struct {
+		line string
+	}
+	tests := []struct {
+		name string
+		args args
+		want *Puzzle
+	}{
+		{
+			name: "ok",
+			args: args{
+				line: "156: 15 6",
+			},
+			want: &Puzzle{
+				Result:  156,
+				Numbers: []int{15, 6},
+			},
+		},
+		{
+			name: "ok",
+			args: args{
+				line: "7290: 6 8 6 15",
+			},
+			want: &Puzzle{
+				Result:  7290,
+				Numbers: []int{6, 8, 6, 15},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ParseLine(tt.args.line); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ParseLine() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
